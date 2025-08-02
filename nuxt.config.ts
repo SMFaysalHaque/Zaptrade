e4b84ad9a6442427402437a46a622273ea997de9
SMFaysalHaque <smfaysalhaque007@gmail.com>
2025-08-02 12:36:26 +0600

    project environment setup complete and adding video, images etc.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
   modules: ['@nuxt/content', '@nuxt/eslint']
 })
